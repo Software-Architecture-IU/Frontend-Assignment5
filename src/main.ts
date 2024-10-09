ecf8bc3152e14bccc69863108ae61eb4a105a3d8
Dan <whatdanman@gmail.com>
2024-10-09 23:31:37 +0300

    chore: add new features

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-const POSTS_URL = "http://tw-posts.stress-testers.ru";
-const LIKES_URL = "http://tw-likes.stress-testers.ru";
-const POLL_INTERVAL = 1000;
-const LIKE_ICON =
-  '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>';
+import {
+  LIKES_URL,
+  POLL_POSTS_INTERVAL,
+  POLL_LIKES_INTERVAL,
+  Post,
+  POSTS_URL,
+  AUTH_URL,
+  Like,
+} from "./const";
+import { buildPost } from "./utils/buildPost";
+import { sendMessage } from "./utils/sendMessage";
 
 const app = document.getElementById("root");
 const sendButton = document.getElementById("send-tw");
 const updateButton = document.getElementById("get-tw");
+const logoutButton = document.getElementById("logout-tw");
 
 const form = document.getElementById("sender");
 function handleForm(event: Event) {
@@ -14,33 +21,27 @@ function handleForm(event: Event) {
 }
 form?.addEventListener("submit", handleForm);
 
-let currentOffset = 0;
-const posts: Post[] = [{ id: 0, username: "anonymous", content: "aboba" }];
-const likes: Likes = { 0: ["anonymous"] };
+let posts: Post[] = [];
 
-interface Post {
-  id: number;
-  username: string;
-  content: string;
-}
-
-interface Likes {
-  [id: number]: string[];
-}
-
-const usernameStorage = window.localStorage.getItem("username");
-
-function pollServer() {
-  fetch(`${POSTS_URL}/posts?offset=${currentOffset}`)
+function pollPostsServer() {
+  const usernameStorage = window.localStorage.getItem("username");
+  fetch(`${POSTS_URL}/posts?username=${usernameStorage!}`)
     .then((res) => res.json())
     .then((data: Post[]) => {
       if (data && data.length) {
-        posts.concat(data);
+        const postsToRemove = posts.filter(
+          (x) => !data.some((y) => y.id == x.id)
+        );
+        for (const post of postsToRemove) {
+          const oldPost = document.getElementById(`tw-post-${post.id}`);
+          oldPost?.remove();
+        }
+        const newPosts = data.filter((x) => !posts.some((y) => y.id == x.id));
         const newPost = new CustomEvent("post", {
-          detail: { posts: data },
+          detail: { posts: newPosts },
         });
+        posts = data;
         app!.dispatchEvent(newPost);
-        currentOffset = data[data.length - 1].id;
       }
     })
     .catch((err) => {
@@ -53,130 +54,172 @@ function pollServer() {
     });
 }
 
-function handleSend() {
-  const postInput = document.getElementById("tw-input") as HTMLInputElement;
-  const content = postInput.value;
+function pollLikesServer() {
+  const usernameStorage = window.localStorage.getItem("username");
   fetch(
-    `${POSTS_URL}/posts?${new URLSearchParams({ username: usernameStorage! })}`,
+    `${LIKES_URL}/likes?${new URLSearchParams({
+      username: usernameStorage!,
+    })}`,
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ content }),
-    }
-  ).catch((err) => {
-    console.error("Failed to create a post:");
-    throw err;
-  });
-  postInput.value = "";
-}
-
-function handleLike(post_id: number) {
-  fetch(
-    `${LIKES_URL}/like?${new URLSearchParams({ username: usernameStorage! })}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ post_id }),
+      body: JSON.stringify({
+        post_ids: posts.map((x) => x.id),
+      }),
     }
   )
-    .then((res) => {
-      const like = document.getElementById("tw-like-" + post_id);
-      if (res.body) {
-        like!.className = "btn btn-secondary tw-like tw-like-active";
-      } else {
-        like!.className = "btn btn-secondary tw-like";
-      }
-    })
-    .catch((err) => {
-      console.error("Failed to like a post:");
-      throw err;
+    .then((res) => res.json())
+    .then((data: { items: Like[] }) => {
+      console.log(data.items);
+      data.items.forEach((x) => {
+        console.log(x);
+        const curLike = document.getElementById(`tw-like-${x.post_id}`);
+        if (x.usernames.includes(usernameStorage!)) {
+          curLike!.className = `btn btn-secondary tw-like tw-like-active`;
+        } else {
+          curLike!.className = `btn btn-secondary tw-like`;
+        }
+
+        const curLikeCount = document.getElementById(
+          `tw-like-${x.post_id}-count`
+        );
+        console.log(curLikeCount);
+        if (x.usernames.length !== parseInt(curLikeCount!.innerText)) {
+          //curLike?.removeChild(curLikeCount!);
+          curLikeCount!.innerText = x.usernames.length.toString();
+          //curLike?.appendChild(curLikeCount!);
+        }
+      });
     });
 }
 
-sendButton?.addEventListener("click", handleSend);
-updateButton?.addEventListener("click", pollServer);
+sendButton?.addEventListener("click", sendMessage);
+updateButton?.addEventListener("click", pollPostsServer);
+logoutButton?.addEventListener("click", () => {
+  window.localStorage.removeItem("username");
+  window.location.replace("/auth");
+});
 
-// function formatTime(time: string) {
-//   // 12:49 | 12 Jun 2024
-//   const months = [
-//     "Jan",
-//     "Feb",
-//     "Mar",
-//     "Apr",
-//     "May",
-//     "Jun",
-//     "Jul",
-//     "Aug",
-//     "Sep",
-//     "Oct",
-//     "Nov",
-//     "Dec",
-//   ];
-//   const date = new Date(time);
-//   const day = String(date.getDate()).padStart(2, "0");
-//   const month = months[date.getMonth()];
-//   const year = date.getFullYear();
-//   return `${date.getHours().toString().padStart(2, "0")}:${date
-//     .getMinutes()
-//     .toString()
-//     .padStart(2, "0")} | ${day} ${month} ${year}`;
-// }
+// @ts-expect-error addEventListener does not have overload with CustomEvent event type
+app!.addEventListener(
+  "like",
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  function (_event: CustomEvent<{ post_id: number }>) {
+    pollLikesServer();
+  }
+);
 
 // @ts-expect-error addEventListener does not have overload with CustomEvent event type
 app!.addEventListener("post", function (event: CustomEvent<{ posts: Post[] }>) {
   const postList = document.getElementById("tw-list");
   for (const post of event.detail.posts) {
-    const newPost = document.createElement("div");
-    newPost.className = "tw";
-
-    const newPostHeader = document.createElement("div");
-    newPostHeader.className = "tw-header";
-
-    const postUsername = document.createElement("span");
-    postUsername.appendChild(document.createTextNode(post.username));
-    postUsername.className = "tw-username";
-
-    const postContent = document.createElement("span");
-    postContent.appendChild(document.createTextNode(post.content));
-    postContent.className = "tw-txt";
-
-    const postLike = document.createElement("button");
-    postLike.className = "btn btn-secondary tw-like";
-    postLike.id = "tw-like-" + post.id;
-
-    const parser = new DOMParser();
-    const likeIcon = parser.parseFromString(LIKE_ICON, "image/svg+xml");
-    const likeIconSvg = likeIcon.documentElement;
-    postLike.appendChild(likeIconSvg);
-    postLike.appendChild(
-      document.createTextNode(likes[post.id]?.length.toString() || "0")
-    );
-    postLike.addEventListener("click", () => handleLike(post.id));
-
-    //   const postTime = document.createElement("span");
-    //   postTime.appendChild(document.createTextNode(formatTime(post.timestamp)));
-    //   postTime.className = "tw-time";
-
-    //newPostHeader.appendChild(postUsername);
-    //newPostHeader.appendChild(postTime);
-
-    newPost.appendChild(postUsername);
-    newPost.appendChild(postContent);
-    newPost.appendChild(postLike);
+    const newPost = buildPost(post);
     postList?.appendChild(newPost);
   }
 });
 
-// function checkAuth() {
-//   if (!usernameStorage) {
-//     window.location.replace("/auth");
-//   }
-// }
+function checkAuth() {
+  const usernameStorage = window.localStorage.getItem("username");
+  if (!usernameStorage) {
+    window.location.replace("/auth");
+  }
+}
 
-//checkAuth();
-pollServer();
-setInterval(pollServer, POLL_INTERVAL);
+if (
+  !window.location.pathname.includes("/auth") &&
+  !window.location.pathname.includes("/register")
+) {
+  const usernameStorage = window.localStorage.getItem("username");
+  const usernameHeader = document.getElementById("tw-username-header");
+  usernameHeader!.innerText = `Welcome, ${usernameStorage}!`;
+  checkAuth();
+  pollPostsServer();
+  pollLikesServer();
+  setInterval(pollPostsServer, POLL_POSTS_INTERVAL);
+  setInterval(pollLikesServer, POLL_LIKES_INTERVAL);
+}
+
+// Auth
+const sendAuthButton = document.getElementById("send-auth-tw");
+const usernameAuthInput = document.getElementById("tw-input-auth-username");
+
+function handleAuth(e: MouseEvent) {
+  e.preventDefault();
+  const username = (usernameAuthInput as HTMLInputElement).value;
+  if (username) {
+    fetch(`${AUTH_URL}/auth`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+      }),
+    })
+      .then((res) => {
+        if (res.status >= 500) {
+          alert("Internal Server Error. Sorry for inconvenience.");
+          throw new Error("Internal Server Error.");
+        }
+        if (res.status >= 400) {
+          alert("Incorrect username. Please specify another one.");
+          throw new Error("Incorrect username.");
+        }
+      })
+      .then(() => {
+        window.localStorage.setItem("username", username);
+        window.location.replace("/");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+}
+
+if (sendAuthButton) {
+  sendAuthButton.addEventListener("click", (e) => handleAuth(e));
+}
+
+// Register
+const sendRegisterButton = document.getElementById("send-register-tw");
+const usernameRegisterInput = document.getElementById(
+  "tw-input-register-username"
+);
+
+function handleRegister(e: MouseEvent) {
+  e.preventDefault();
+  const username = (usernameRegisterInput as HTMLInputElement).value;
+  if (username) {
+    fetch(`${AUTH_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+      }),
+    })
+      .then((res) => {
+        if (res.status >= 500) {
+          alert("Internal Server Error. Sorry for inconvenience.");
+          throw new Error("Internal Server Error.");
+        }
+        if (res.status >= 400) {
+          alert("Incorrect username. Please specify another one.");
+          throw new Error("Incorrect username.");
+        }
+      })
+      .then(() => {
+        window.location.replace("/auth");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+}
+
+if (sendRegisterButton) {
+  sendRegisterButton.addEventListener("click", (e) => handleRegister(e));
+}
